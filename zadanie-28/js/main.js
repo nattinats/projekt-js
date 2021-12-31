0d55ec1a5dc83748412fe7814b6e83cc69e7d51c
n.szajner@gmail.com <n.szajner@gmail.com>
2021-12-31 10:51:02 +0100

    zadanie-28: warunki

---
+++ zadanie-28/js/main.js
@@ -0,0 +1,20 @@
+let number = '12'
+
+if (number > 10) {
+  console.log("Podana liczba jest większa od 10");
+} else {
+  console.log("Podana liczba nie jest większa od 10");
+}
+
+
+// moj przyklad testowy, latwiej zrozumiec
+
+let in_stock_yeezy_500 = 512;
+
+if (in_stock_yeezy_500 < 100) {
+  console.log("Time to order more shoes");
+} else {
+  console.log("We have enough shoes in stock");
+}
+
+
