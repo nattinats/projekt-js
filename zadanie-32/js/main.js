5cb4279dd71a2a06712886e4de92cac77113d318
nattinats <n.szajner@gmail.com>
2022-01-03 20:31:40 +0100

    second commit - zadanie 32

--- zadanie-32/js/main.js
+++ zadanie-32/js/main.js
@@ -1,12 +1,28 @@
 let num1 = 2;
 let num2 = 5;
 
+function return_largest_number(a, b) {
+  if (a > b) {
+    let result = a;
+  } else {
+    let result = b;
+  }
 
-if (num1 > num2) {
-  console.log(num1);
-} else {
-  console.log(num2);
+  return result;
 }
 
-//rozwiazanie drugie
+console.log(return_largest_number(num1, num2));
 
+
+
+function return_largest_number(num1, num2) {
+  if (num1 > num2) {
+    let result = num1;
+  } else {
+    let result = num2;
+  }
+
+  return result;
+}
+
+console.log(return_largest_number(num1, num2));
